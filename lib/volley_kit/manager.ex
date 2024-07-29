5263d602e85b6301f6aa6f0ac90591b3524e3a3e
Timothy Davis <tmthydvs@gmail.com>
2024-07-30 09:28:37 +0930

    Get syncing working between views

--- lib/volley_kit/manager.ex
+++ lib/volley_kit/manager.ex
@@ -8,96 +8,73 @@ defmodule VolleyKit.Manager do
 
   alias VolleyKit.Manager.ScratchMatch
 
-  @doc """
-  Returns the list of scratch_matches.
-
-  ## Examples
-
-      iex> list_scratch_matches()
-      [%ScratchMatch{}, ...]
-
-  """
   def list_scratch_matches do
     Repo.all(ScratchMatch)
   end
 
-  @doc """
-  Gets a single scratch_match.
-
-  Raises `Ecto.NoResultsError` if the Scratch match does not exist.
-
-  ## Examples
-
-      iex> get_scratch_match!(123)
-      %ScratchMatch{}
-
-      iex> get_scratch_match!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_scratch_match!(id), do: Repo.get!(ScratchMatch, id)
 
-  @doc """
-  Creates a scratch_match from its options, given a creator's ID.
-
-  ## Examples
-
-      iex> create_scratch_match(user_id, %{field: value})
-      {:ok, %ScratchMatch{}}
-
-      iex> create_scratch_match(user_id, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_scratch_match(created_by, options \\ %{}) do
     %ScratchMatch{created_by: created_by, a_score: 0, b_score: 0}
     |> ScratchMatch.changeset(%{"options" => options})
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a scratch_match.
-
-  ## Examples
-
-      iex> update_scratch_match(scratch_match, %{field: new_value})
-      {:ok, %ScratchMatch{}}
-
-      iex> update_scratch_match(scratch_match, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_scratch_match(%ScratchMatch{} = scratch_match, attrs) do
     scratch_match
     |> ScratchMatch.changeset(attrs)
     |> Repo.update()
   end
 
+  defp topic(%ScratchMatch{id: id}), do: "scratch_match:#{id}"
+
   @doc """
-  Deletes a scratch_match.
+  Give a point to team `a` or team `b`, and broadcast the `score` event.
 
-  ## Examples
+  The broadcast is done to the "scratch_match:\<ID\>" topic.
+  The event payload is a map of the changes made to the match.
 
-      iex> delete_scratch_match(scratch_match)
-      {:ok, %ScratchMatch{}}
+  ## Example
 
-      iex> delete_scratch_match(scratch_match)
-      {:error, %Ecto.Changeset{}}
+      iex> match = %ScratchMatch{id: 2, a_score: 12, b_score: 13}
+
+      iex> score_scratch_match(match, "a")
+      {:ok, %ScratchMatch{...}}
+
+
+  The above will broadcast a `score` event to the `scratch_match:2` topic
+  with the following payload:
+
+      %{a_score: 13}
+
+
+  This can be used to merge with the current match in a LiveView.
 
   """
+  def score_scratch_match(%ScratchMatch{} = scratch_match, team) when team in ~w(a b) do
+    atom = String.to_atom(team <> "_score")
+    current = Map.get(scratch_match, atom, 0)
+
+    update_map = %{atom => current + 1}
+
+    with {:ok, scratch_match} <- update_scratch_match(scratch_match, update_map) do
+      VolleyKitWeb.Endpoint.broadcast(topic(scratch_match), "score", update_map)
+      {:ok, scratch_match}
+    end
+  end
+
+  def subscribe_scratch_match(scratch_match) do
+    VolleyKitWeb.Endpoint.subscribe(topic(scratch_match))
+  end
+
+  def unsubscribe_scratch_match(scratch_match) do
+    VolleyKitWeb.Endpoint.unsubscribe(topic(scratch_match))
+  end
+
   def delete_scratch_match(%ScratchMatch{} = scratch_match) do
     Repo.delete(scratch_match)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking scratch_match changes.
-
-  ## Examples
-
-      iex> change_scratch_match(scratch_match)
-      %Ecto.Changeset{data: %ScratchMatch{}}
-
-  """
   def change_scratch_match(%ScratchMatch{} = scratch_match, attrs \\ %{}) do
     ScratchMatch.changeset(scratch_match, attrs)
   end
--- lib/volley_kit_web/live/scratch_match_live.ex
+++ lib/volley_kit_web/live/scratch_match_live.ex
@@ -1,27 +1,38 @@
 defmodule VolleyKitWeb.ScratchMatchLive do
-  alias VolleyKit.Manager.ScratchMatch
   use VolleyKitWeb, :live_view
 
   alias VolleyKit.Manager
+  alias VolleyKit.Manager.ScratchMatch
 
-  def mount(%{"id" => id} = _params, %{"user_id" => user_id} = _session, socket) do
+  def mount(_params, %{"user_id" => user_id} = _session, socket) do
+    {:ok, assign(socket, user_id: user_id), layout: false}
+  end
+
+  def handle_params(%{"id" => id} = _params, _uri, socket) do
     match = Manager.get_scratch_match!(id)
-    scorer? = match.created_by == user_id
+    scorer? = match.created_by == socket.assigns.user_id
 
-    socket = socket |> assign(match: match, scorer?: scorer?)
+    if connected?(socket) do
+      if old_match = socket.assigns[:match] do
+        Manager.unsubscribe_scratch_match(old_match)
+      end
 
-    {:ok, socket, layout: false}
+      Manager.subscribe_scratch_match(match)
+    end
+
+    {:noreply, assign(socket, match: match, scorer?: scorer?)}
+  end
+
+  def handle_info(%{event: "score", payload: update_map}, %{assigns: %{match: match}} = socket) do
+    {:noreply, assign(socket, match: Map.merge(match, update_map))}
   end
 
   def handle_event("score", %{"team" => team}, socket) do
     {:noreply, score(socket, socket.assigns.match, team)}
   end
 
-  def score(socket, %ScratchMatch{} = match, team) when team in ~w(a b) do
-    atom = String.to_atom(team <> "_score")
-    current_score = Map.get(match, atom, 0)
-
-    {:ok, match} = Manager.update_scratch_match(match, %{atom => current_score + 1})
+  def score(socket, %ScratchMatch{} = match, team) do
+    {:ok, match} = Manager.score_scratch_match(match, team)
 
     assign(socket, match: match)
   end

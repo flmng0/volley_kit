d90c410f1d811c3d44a504f8b2180f3594eca77f
Timothy Davis <tmthydvs@gmail.com>
2024-12-12 00:00:38 +0930

    Remove unused things

--- lib/volley_web/live/home_live.ex
+++ lib/volley_web/live/home_live.ex
@@ -5,9 +5,4 @@ defmodule VolleyWeb.HomeLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
-
-  @impl true
-  def handle_event("test", _params, socket) do
-    {:noreply, put_flash(socket, :info, "THis is a test")}
-  end
 end

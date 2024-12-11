35220ddc690dc1f48f0eb543af71cd426f587a60
Timothy Davis <tmthydvs@gmail.com>
2024-12-12 00:06:29 +0930

    Comment out unused test

--- test/volley_web/controllers/page_controller_test.exs
+++ test/volley_web/controllers/page_controller_test.exs
@@ -1,8 +1,9 @@
 defmodule VolleyWeb.PageControllerTest do
   use VolleyWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
+  # Out of date and not ready to test
+  # test "GET /", %{conn: conn} do
+  #   conn = get(conn, ~p"/")
+  #   assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  # end
 end

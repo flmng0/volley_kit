c12e60b58539bc87ceace0dfb9171bd116a653c7
Timothy Davis <tmthydvs@gmail.com>
2024-12-11 23:19:57 +0930

    Simplify seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -13,9 +13,11 @@
 alias Volley.Repo
 
 if Application.compile_env!(:volley, :dev_routes) do
-  {:ok, match} = Volley.create_match()
+  {:ok, match} = Volley.create_match(%{team_a_name: "a", team_b_name: "b", set_point_limit: 25})
 
-  {:ok, match} = Volley.score_match(match, :a)
-  {:ok, match} = Volley.score_match(match, :a)
-  {:ok, _match} = Volley.score_match(match, :a)
+  for _ <- 1..3, reduce: match do
+    match ->
+      {:ok, match} = Volley.score_match(match, :a)
+      match
+  end
 end

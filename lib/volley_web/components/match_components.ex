7c5b7b02e8c03a9f5ac30eb94c6e8e5dd4c0e75b
Timothy Davis <tmthydvs@gmail.com>
2025-07-21 01:50:52 +0930

    SVG for full-sized text and more scaffolding

--- lib/volley_web/components/match_components.ex
+++ lib/volley_web/components/match_components.ex
@@ -1,34 +1,51 @@
 defmodule VolleyWeb.MatchComponents do
   use Phoenix.Component
 
-  # alias Phoenix.LiveView.JS
+  attr :a_score, :integer, required: true
+  attr :b_score, :integer, required: true
+
+  attr :event, :string, required: true
+
+  def score_container(assigns) do
+    ~H"""
+    <div class="grid landscape:grid-cols-2 portrait:grid-rows-2 h-screen">
+      <.score_card team={:a} score={@a_score} phx-click={@event} />
+      <.score_card team={:b} score={@b_score} phx-click={@event} />
+    </div>
+    """
+  end
 
   attr :score, :integer, required: true
   attr :team, :atom, values: [:a, :b], required: true
 
   attr :rest, :global
 
-  def score_card(assigns) do
+  defp score_card(assigns) do
     team_class =
       case assigns[:team] do
-        :a -> "bg-(--color-team-a)"
-        :b -> "bg-(--color-team-b)"
+        :a -> "bg-team-a"
+        :b -> "bg-team-b"
       end
 
     assigns = assign(assigns, :class, team_class)
 
     ~H"""
-    <button class={[@class]} {@rest}>
-      Test {@score}
+    <button
+      class={["flex flex-col justify-center max-h-screen cursor-pointer", @class]}
+      phx-value-team={@team}
+      {@rest}
+    >
+      <svg
+        viewBox="0 0 24 14"
+        stroke="none"
+        preserveAspectRatio="true"
+        width="100%"
+        height="100%"
+        fill="currentColor"
+      >
+        <text x="50%" y="50%" dominant-baseline="central" text-anchor="middle">{@score}</text>
+      </svg>
     </button>
-    """
-  end
-
-  slot :inner_block, required: true
-
-  def score_container(assigns) do
-    ~H"""
-    <div class="flex landscape:flex-row portrait:flex-col"></div>
     """
   end
 end
--- lib/volley_web/live/scratch_match_live.ex
+++ lib/volley_web/live/scratch_match_live.ex
@@ -2,12 +2,28 @@ defmodule VolleyWeb.ScratchMatchLive do
   use VolleyWeb, :live_view
   import VolleyWeb.MatchComponents
 
+  @impl true
+  def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:a_score, 0)
+      |> assign(:b_score, 0)
+
+    {:ok, socket}
+  end
+
+  @impl true
   def render(assigns) do
     ~H"""
-    <Layouts.app current_scope={@current_scope} flash={@flash}>
-      <.score_card score={0} team={:a} />
-      <.score_card score={0} team={:b} />
-    </Layouts.app>
+    <.score_container a_score={@a_score} b_score={@b_score} event="score" />
     """
+  end
+
+  @impl true
+  def handle_event("score", %{"team" => team}, socket) do
+    team_score = String.to_existing_atom("#{team}_score")
+    socket = assign(socket, team_score, socket.assigns[team_score] + 1)
+
+    {:noreply, socket}
   end
 end

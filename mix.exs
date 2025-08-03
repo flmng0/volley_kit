9396e997a18cae3007e55aace68c71ec4e79f941
Timothy Davis <tmthydvs@gmail.com>
2025-08-03 15:53:48 +0930

    Upgrade :phoenix_live_view

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Volley.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.1.0-rc.0"},
+      {:phoenix_live_view, "~> 1.1"},
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
